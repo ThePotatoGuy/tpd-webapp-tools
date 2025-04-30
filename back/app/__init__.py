e7019fe2f26bbc73fe255015892544b87ce98ff6
potato <potato@desu.zone>
2025-04-30 02:22:40 -0500

    update stuff and stuff

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app = Flask(__name__, static_folder='../static', static_url_path='/')
     CORS(app)  # Enable CORS if frontend is hosted separately in dev
 
-    from .routes.api import api_bp
+    from routes.api import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
